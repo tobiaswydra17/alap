e01a6b13e28b3bed0caeb041b40f49eda62f523d
tobia <tobias.wydra@outlook.com>
2026-03-26 17:13:04 +0100

    Initial commit

---
+++ src/main/java/at/fhtw/alap/AlapApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.alap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhtw/alap/AlapApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.alap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
